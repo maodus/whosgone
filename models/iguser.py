f31ad224c8fb722e22d7e14f9d81e52bffa996a5
maodus <maodus@ualberta.ca>
2023-09-20 23:58:45 -0600

    Added a model for representing an instagram user and created the main file for the program.

---
+++ models/iguser.py
@@ -0,0 +1,13 @@
+# Represents an instagram user that is followed by or is following you
+class IGUser:
+    def __init__(self, name, interaction_date):
+        self.name = name
+        self.interaction_date = interaction_date
+
+    def __eq__(self, other):
+        if not isinstance(other, IGUser):
+            return False
+        return self.name == other.name
+    
+    def __hash__(self):
+        return hash(self.name)
