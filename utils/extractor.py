1abd9100229cfa214858f5178df1e0de71b26414
maodus <maodus@ualberta.ca>
2023-09-21 02:49:48 -0600

    Added a utility for extracting data out of the instagram-provided data file. More specifically, this class will extract the user's following and follower data.

---
+++ utils/extractor.py
@@ -0,0 +1,24 @@
+from zipfile import ZipFile
+from . import jsontools
+
+class IGExtractor:
+    
+    # path should point to the location of the instagram provided zip-file
+    def __init__(self, path):
+        self.path = path
+    
+    # Reads the content of a zipped file and returns them
+    # sub_path represents a file path to read from that is relative to the zip file
+    def __read_zipped_file(self, sub_path):
+        with ZipFile(self.path) as zip:
+            with zip.open(sub_path) as file:
+                return file.read()
+    
+    def get_followers(self):
+        data = self.__read_zipped_file("followers_and_following/followers.json")
+        return jsontools.instantiate_users(data, "relationships_followers")
+
+
+    def get_following(self):
+        data = self.__read_zipped_file("followers_and_following/following.json")
+        return jsontools.instantiate_users(data, "relationships_following")
